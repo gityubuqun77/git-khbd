3d19d4379f1b4e7e3cd13a95bede4a6ed720aa36
song_wen_pan <song_wen_pan@163.com>
2021-12-09 16:07:57 +0800

    idea git 初始化提交

---
+++ src/main/java/com/swp/gitkhbd/GitKhbdApplication.java
@@ -0,0 +1,13 @@
+package com.swp.gitkhbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitKhbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitKhbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swp/gitkhbd/GitKhbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp.gitkhbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitKhbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
